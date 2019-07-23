16755359dbb55f41948524d7b4fece989a41346e
Chris <50714992+cmpowers1981@users.noreply.github.com>
2019-07-23 10:32:55 -0400

    JS Upload

---
+++ src/src/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+$(document).ready(function() {
+    shoppingList.bindEventListeners();
+    shoppingList.render();
+    api.getItems((items)=>{
+      items.forEach((item) => store.addItem(item));
+      /* test code */
+      // const item = store.items[0];
+      // console.log('current name: ' + item.name);
+      // store.findAndUpdate(item.id, { name: 'stuff' });
+      // console.log('new name: ' + item.name);
+      shoppingList.render();
+    });
+  });
+
+
+
